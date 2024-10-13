3c43bd27e7e3e4db6666b8b71ae49a02d08e9257
Naur4h <naurah.claradinda24@mhs.uinjkt.ac.id>
2024-10-13 16:26:18 +0700

    Create test.css

---
+++ test.css
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+    <style> 
+        body {
+            margin-inline: 1.5 rem;
+        }
+
+        table {
+            width: 100%;
+            border: 20px solid #e2ace9;
+        }
+        
+        tr:nth-child(even) {
+            background-color: rgb(196, 235, 151);
+        }
+        th, td {
+            text-align: left;
+            padding: 1rem;
+        }
+    </style>
+</head>
+<body>
+    <h2>Daftar Mahasiswa</h2>
+    <br>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Foto</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Levi</td>
+            <td>Pria</td>
+            <td>
+                <img src="file:///C:/Users/User/Downloads/levi.jpg" width="100px" alt="">
+            </td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Rossa</td>
+            <td>Wanita</td>
+            <td>
+                <img src="file:///C:/Users/User/Downloads/collage-2023-02-02T072325.349.jpg" width="100px" alt="">
+            </td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Raisa</td>
+            <td>Wanita</td>
+            <td>
+                <img src="file:///C:/Users/User/Downloads/WIbmjnRkZx.jpg" width="100px" alt="">
+            </td>
+        </tr>
+    </table>
+    
+</body>
+</html>
